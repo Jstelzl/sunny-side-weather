bcf62714050d3cb5d74ea2ae3ec3cbae7b96a09e
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-07 12:18:13 -0400

    first push, worked on api fetch

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+let weather = {
+    "apiKey": "f6634c74e1ebddea5c471376cbdb35fc",
+    fetchWeather: function(city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=metric&appid=" 
+        + this.appKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name} = data; 
+        const { icon, description } = data.weather;
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed); 
+        document.querySelector("city").innerText = "Wather in" + name;
+    }
+};

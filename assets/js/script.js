166414b54de057755c5f0eded967a41f57543472
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-10-08 11:22:41 -0400

    reworked api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,48 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-
-let weather = {
-    "apiKey": "f6634c74e1ebddea5c471376cbdb35fc",
-    fetchWeather: function(city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
-        + city 
-        + "&units=metric&appid=" 
-        + this.appKey
+var currentWeather = document.getElementById("current-weather");
+console.log(currentWeather);
+var forecastWeather = document.getElementById("forecast-weather");
+var apiKey = 'f6634c74e1ebddea5c471376cbdb35fc';
+function fetchWeather() {
+    var lat = '35.2271';
+    var long = '-80.8431';
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long 
+        + "&units=imperial&appid=" 
+        + apiKey
         )
         .then((response) => response.json())
-        .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function(data) {
-        const { name} = data; 
-        const { icon, description } = data.weather;
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        console.log(name,icon,description,temp,humidity,speed); 
-        document.querySelector("city").innerText = "Wather in" + name;
-    }
+        .then((data) => {
+            displayCurrent(data)
+            displayForecast(data)
+        });
 };
+
+function displayCurrent(data) {
+    console.log(data);
+    var cityName = document.createElement("h3");
+    cityName.textContent = data.name
+    currentWeather.appendChild(cityName);
+    var temp = document.createElement("p");
+    temp.textContent = data.main.temp
+    currentWeather.appendChild(temp);
+    // const { name } = data; 
+    // const { icon, description } = data.;
+    // const { temp, humidity } = data.;
+    // const { speed } = data.wind;
+    // console.log(name,icon,description,temp,humidity,speed); 
+    // document.querySelector(".city").innerText = "Wather in" + name;
+    // document.querySelector(".icon").src = 
+    //     "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+};
+
+function displayForecast(data) {
+
+};
+
+function getCord() {
+
+};
+
+fetchWeather();
